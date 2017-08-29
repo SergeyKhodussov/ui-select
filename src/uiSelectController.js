a9639ebd87cf6736a68a274be8b29923223f8663
SergeyKhodussov <sergmastx@gmail.com>
2017-08-29 16:34:33 +0600

    Bug, which led to the moving of the search input to next line
    Bug, which led to the moving of the search input to next line!
    
    This change fixes a bug!

--- src/uiSelectController.js
+++ src/uiSelectController.js
@@ -545,7 +545,7 @@ uis.controller('uiSelectCtrl',
           }
           var inputWidth = containerWidth - input.offsetLeft;
           if (inputWidth < 50) inputWidth = containerWidth;
-          ctrl.searchInput.css('width', inputWidth+'px');
+          ctrl.searchInput.css('width', (inputWidth - 5)+'px');
           return true;
         };
 
